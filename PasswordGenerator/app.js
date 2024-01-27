4f91eed4a11ed3a9cc1d3f5f24da11336ce2f0f6
SHARIQ SHAHOOD <shariqshahood43@gmail.com>
2024-01-27 02:12:11 -0800

    fb

--- PasswordGenerator/app.js
+++ PasswordGenerator/app.js
@@ -1,11 +1,18 @@
-var length = +prompt("Enter The Length Of Password: "); // Set the desired length of the password
+// var length = +prompt("Enter The Length Of Password: "); // Set the desired length of the password
 
-function generatePassword() {
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"; // Include the characters you want in the password
-    let password = "";
-    for (let i = 0; i < length; i++) {
-        var randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset.charAt(randomIndex);
+// function generatePassword() {
+//     var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"; // Include the characters you want in the password
+//     let password = "";
+//     for (let i = 0; i < length; i++) {
+//         var randomIndex = Math.floor(Math.random() * charset.length);
+//         password += charset.charAt(randomIndex);
+//     }
+//     document.getElementById('password').textContent = password;
+// }
+arr=[1,2,3,4,5,6,8,9,10];
+for (var i=0;i<arr.length;i++) {
+    if(arr[i+1]-arr[i] === 2){
+       document.write(i+"is missing");
     }
-    document.getElementById('password').textContent = password;
+    
 }
---
+++ fb-post/app.js
@@ -0,0 +1,103 @@
+
+function setBackground() {
+    var postBackground = document.getElementById('post-background');
+    postBackground.style.backgroundImage = 'url(' + event.target.src + ')'
+    // console.log(event.target)
+
+}
+function changeColor() {
+    var postBackground = document.getElementById('post-background');
+    postBackground.style.color = event.target.value;
+    // console.log(event.target.value)
+}
+function changeSize() {
+    var postBackground = document.getElementById('post-background');
+    postBackground.style.fontSize = event.target.value + "px";
+    // console.log(event.target.value)
+}
+function changeStyle() {
+    var postBackground = document.getElementById('post-background');
+    var type = event.target.innerText;
+    if (type === "B") {
+        if (postBackground.style.fontWeight === "bold") {
+            postBackground.style.fontWeight = 'normal';
+            event.target.style.color = 'black';
+        }
+        else {
+            postBackground.style.fontWeight = 'bold';
+            event.target.style.color = 'red';
+
+        }
+    }
+    else if (type === "𝐼") {
+        if (postBackground.style.fontStyle === "italic") {
+            postBackground.style.fontStyle = 'normal';
+            event.target.style.color = 'black';
+
+        }
+        else {
+            postBackground.style.fontStyle = 'italic';
+            event.target.style.color = 'red';
+
+        }
+    }
+    else if (type === "U") {
+        if (postBackground.style.textDecoration === "underline") {
+            postBackground.style.textDecoration = 'none';
+            event.target.style.color = 'black';
+
+        }
+        else {
+            postBackground.style.textDecoration = 'underline';
+            event.target.style.color = 'red';
+
+        }
+    }
+
+}
+function cut() {
+
+    var alls = document.getElementById('all')
+
+    alls.style.display = 'none';
+    document.write("<center><h1>" + 'Refresh the page..' + "</h1></center>")
+
+}
+function enterComment() {
+    var com = document.getElementById('comment-section');
+    com.style.display = 'contents';
+}
+function share() {
+    document.write("<h1>" + "Share Succesfully.." + "</h1>");
+}
+// function like(){
+//     var lik = document.getElementById('liking');
+//     event.target.style.display='contents';
+// }
+function createPost() {
+    var item = document.getElementById('item');
+    var head = document.getElementById('head');
+    var head2 = document.getElementById('head2');
+    var react = document.getElementById('rc');
+    var react2 = document.getElementById('rc2');
+    var react3 = document.getElementById('rc3');
+
+
+    item.style.display = 'none';
+    head.style.display = 'none';
+    head2.style.display = 'contents';
+    react.style.display = 'contents';
+    react2.style.display = 'contents';
+    react3.style.display = 'contents';
+
+
+
+
+
+
+}
+function setEmoji() {
+    var postBackground = document.getElementById('post-background')
+    postBackground.value += event.target.value;
+    // console.log(event.target.value)
+}
